321919547793aa8e6f843fdb6dfbe0cc980fe2c7
NadyaPod <nadegda.podyacheva@gmail.com>
2024-03-22 17:44:31 +0100

    feat: fetch

--- script.js
+++ script.js
@@ -4,28 +4,28 @@ const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
-const getCountyData = (country) => {
-  const request = new XMLHttpRequest();
-  request.open('GET', `https://restcountries.com/v2/name/${country}`);
-  request.send();
+// const getCountyData = (country) => {
+//   const request = new XMLHttpRequest();
+//   request.open('GET', `https://restcountries.com/v2/name/${country}`);
+//   request.send();
 
-  request.addEventListener('load', function() {
-    const [data] = JSON.parse(this.responseText);
-    const html = `
-    <article class="country">
-    <img class="country__img" src="${data.flag}" />
-    <div class="country__data">
-    <h3 class="country__name">${data.name}</h3>
-    <h4 class="country__region">${data.region}</h4>
-    <p class="country__row"><span>${(+data.population / 1000000).toFixed(1)}👫</span></p>
-    <p class="country__row"><span>${data.languages[0].name}🗣️</span></p>
-    <p class="country__row"><span>${data.currencies[0].name}💰</span></p>
-    </div>
-    </article>`
-    countriesContainer.insertAdjacentHTML('beforeend', html);
-    countriesContainer.style.opacity = 1;
-  })
-}
+//   request.addEventListener('load', function() {
+//     const [data] = JSON.parse(this.responseText);
+//     const html = `
+//     <article class="country">
+//     <img class="country__img" src="${data.flag}" />
+//     <div class="country__data">
+//     <h3 class="country__name">${data.name}</h3>
+//     <h4 class="country__region">${data.region}</h4>
+//     <p class="country__row"><span>${(+data.population / 1000000).toFixed(1)}👫</span></p>
+//     <p class="country__row"><span>${data.languages[0].name}🗣️</span></p>
+//     <p class="country__row"><span>${data.currencies[0].name}💰</span></p>
+//     </div>
+//     </article>`
+//     countriesContainer.insertAdjacentHTML('beforeend', html);
+//     countriesContainer.style.opacity = 1;
+//   })
+// }
 
 
 const renderCountry = (data) => {
@@ -44,30 +44,39 @@ const renderCountry = (data) => {
   countriesContainer.style.opacity = 1;
 }
 
-const getCountyDataAndNeighbour = (country) => {
-  const request = new XMLHttpRequest();
-  request.open('GET', `https://restcountries.com/v2/name/${country}`);
-  request.send();
+// const getCountyDataAndNeighbour = (country) => {
+//   const request = new XMLHttpRequest();
+//   request.open('GET', `https://restcountries.com/v2/name/${country}`);
+//   request.send();
 
-  request.addEventListener('load', function() {
-    const [data] = JSON.parse(this.responseText);
-    console.log(data);
-    renderCountry(data)
-    const neighbour = data.borders?.[0];
-    console.log(neighbour);
+//   request.addEventListener('load', function() {
+//     const [data] = JSON.parse(this.responseText);
+//     console.log(data);
+//     renderCountry(data)
+//     const neighbour = data.borders?.[0];
+//     console.log(neighbour);
 
-    const request2 = new XMLHttpRequest();
-    request2.open('GET', `https://restcountries.com/v2/alpha/${neighbour}`);
-    request2.send();
+//     const request2 = new XMLHttpRequest();
+//     request2.open('GET', `https://restcountries.com/v2/alpha/${neighbour}`);
+//     request2.send();
 
-    request2.addEventListener('load', function() {
-      const data2 = JSON.parse(this.responseText);
-      console.log(data2);
-      renderCountry(data2)
+//     request2.addEventListener('load', function() {
+//       const data2 = JSON.parse(this.responseText);
+//       console.log(data2);
+//       renderCountry(data2)
 
-    })
-  })
+//     })
+//   })
+// }
+
+const request = fetch('https://restcountries.com/v2/name/portugal')
+console.log(request);
+
+const getCountyData = (country) => {
+  fetch(`https://restcountries.com/v2/name/${country}`)
+    .then((resp) => resp.json())
+    .then((data) => renderCountry(data[0]));
 }
 
-getCountyDataAndNeighbour('ukraine ')
+getCountyData('spain')
 
