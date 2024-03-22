9edf67e1ed7b5853f61c02fd8271989d8df8b72f
NadyaPod <nadegda.podyacheva@gmail.com>
2024-03-22 16:26:56 +0100

    feat: countries info

--- script.js
+++ script.js
@@ -4,3 +4,29 @@ const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
+const getCountyData = (country) => {
+  const request = new XMLHttpRequest();
+  request.open('GET', `https://restcountries.com/v2/name/${country}`);
+  request.send();
+
+  request.addEventListener('load', function() {
+    const [data] = JSON.parse(this.responseText);
+    const html = `
+    <article class="country">
+    <img class="country__img" src="${data.flag}" />
+    <div class="country__data">
+    <h3 class="country__name">${data.name}</h3>
+    <h4 class="country__region">${data.region}</h4>
+    <p class="country__row"><span>${(+data.population / 1000000).toFixed(1)}👫</span></p>
+    <p class="country__row"><span>${data.languages[0].name}🗣️</span></p>
+    <p class="country__row"><span>${data.currencies[0].name}💰</span></p>
+    </div>
+    </article>`
+    countriesContainer.insertAdjacentHTML('beforeend', html);
+    countriesContainer.style.opacity = 1;
+  })
+}
+
+getCountyData('portugal')
+getCountyData('usa')
+
